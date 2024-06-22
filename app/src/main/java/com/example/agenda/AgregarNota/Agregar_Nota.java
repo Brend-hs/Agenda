ea0611377dffcb2672e853adb52a987d36ffa79e
MarianaBHdz <mariana.berenice2002@gmail.com>
2024-06-22 13:55:05 -0600

    Agregar nota finalizado

--- app/src/main/java/com/example/agenda/AgregarNota/Agregar_Nota.java
+++ app/src/main/java/com/example/agenda/AgregarNota/Agregar_Nota.java
@@ -1,8 +1,13 @@
 package com.example.agenda.AgregarNota;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,13 +23,17 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.agenda.Objetos.Nota;
 import com.example.agenda.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -32,13 +41,18 @@ import java.util.Locale;
 
 public class Agregar_Nota extends AppCompatActivity {
 
-    TextView Uid_Usuario, Correo_Usuario, Fecha_hora_actual, Fecha, Hora, Estado;
+    TextView Uid_Usuario, Correo_Usuario, Fecha_hora_actual, Fecha, Hora, Estado, Contacto;
     EditText Titulo, Descripcion;
-    Button Btn_Calendario, Btn_Hora;
+    Button Btn_Calendario, Btn_Hora, Btn_Contactos;
 
     int mes, dia, anio, hora, minuto;
 
     Spinner SpinnerNotificacion, SpinnerCategoria;
+
+    int REQUEST_CONTACT = 1;
+    String ContactoSeleccionado = "";
+
+    DatabaseReference BD_Firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +73,96 @@ public class Agregar_Nota extends AppCompatActivity {
         InicializarVariables();
         ObtenerDatos();
         Obtener_Fecha_Hora_Actual();
+        Establecer_Fecha();
+        Establecer_Hora();
+        EstablecerNotificacion();
+        EstabecerCategoria();
+        Obtener_Contacto();
 
+        /*TO-DO:
+        * Poder añadir otra categoria
+        * Añadir contacto*/
+    }
+
+    private void InicializarVariables(){
+        Uid_Usuario = findViewById(R.id.Uid_Usuario);
+        Correo_Usuario= findViewById(R.id.Correo_Usuario);
+        Fecha_hora_actual= findViewById(R.id.Fecha_hora_actual);
+        Fecha = findViewById(R.id.Fecha);
+        Hora = findViewById(R.id.Hora);
+        Estado = findViewById(R.id.Estado);
+        Contacto = findViewById(R.id.Contacto);
+
+        Titulo = findViewById(R.id.Titulo);
+        Descripcion = findViewById(R.id.Descripcion);
+        Btn_Calendario = findViewById(R.id.Btn_Calendario);
+        Btn_Hora = findViewById(R.id.Btn_Hora);
+        Btn_Contactos = findViewById(R.id.Btn_Contactos);
+
+        SpinnerNotificacion = findViewById(R.id.SpinnerNotificacion);
+        SpinnerCategoria = findViewById(R.id.SpinnerCategoria);
+
+        BD_Firebase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void ObtenerDatos(){
+        String uid_recuperado = getIntent().getStringExtra("Uid");
+        String correo_recuperado = getIntent().getStringExtra("Correo");
+
+        Uid_Usuario.setText(uid_recuperado);
+        Correo_Usuario.setText(correo_recuperado);
+    }
+
+    private void Obtener_Fecha_Hora_Actual(){
+        String fecha_hora_registro = new SimpleDateFormat("dd-MM-yyyy/HH:mm:ss a",
+                Locale.getDefault()).format(System.currentTimeMillis());
+        Fecha_hora_actual.setText(fecha_hora_registro);
+    }
+
+    private void Guardar_Nota(){
+        //Obtener los datoss
+        String uid_usuario = Uid_Usuario.getText().toString();
+        String correo_usuario = Correo_Usuario.getText().toString();
+        String fecha_hora_actual = Fecha_hora_actual.getText().toString();
+        String titulo = Titulo.getText().toString();
+        String descripcion = Descripcion.getText().toString();
+        String fecha = Fecha.getText().toString();
+        String hora = Hora.getText().toString();
+        String notificacion = SpinnerNotificacion.getSelectedItem().toString();
+        String categoria = SpinnerCategoria.getSelectedItem().toString();
+        String estado = Estado.getText().toString();
+        
+        //Validar cada uno de los datos
+        if(!uid_usuario.isEmpty() && !correo_usuario.isEmpty() && !fecha_hora_actual.isEmpty() && !titulo.isEmpty() &&
+                !descripcion.isEmpty() && !fecha.isEmpty() && !hora.isEmpty() && !notificacion.isEmpty() &&
+                !categoria.isEmpty() && !estado.isEmpty()){
+            Nota nota = new Nota(correo_usuario+"/"+fecha_hora_actual,
+                    uid_usuario,
+                    correo_usuario,
+                    fecha_hora_actual,
+                    titulo,
+                    descripcion,
+                    fecha,
+                    hora,
+                    notificacion,
+                    categoria,
+                    ContactoSeleccionado,
+                    estado);
+
+            String Nota_Usuario = BD_Firebase.push().getKey();
+            //Establecer el nombre de la base de datos
+            String Nombre_BD = "Notas_Publicadas";
+
+            BD_Firebase.child(Nombre_BD).child(Nota_Usuario).setValue(nota);
+            Toast.makeText(this, "Se ha agregado la tarea exitosamente", Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }else {
+            Toast.makeText(this, "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void Establecer_Fecha(){
         Btn_Calendario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,11 +195,13 @@ public class Agregar_Nota extends AppCompatActivity {
                         Fecha.setText(diaFormateado+"/"+mesFormateado+"/"+AnioSeleccionado);
                     }
                 }
-                ,anio,mes,dia);
+                        ,anio,mes,dia);
                 datePickerDialog.show();
             }
         });
+    }
 
+    private void Establecer_Hora(){
         Btn_Hora.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -145,44 +250,6 @@ public class Agregar_Nota extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
-
-        EstablecerNotificacion();
-        EstabecerCategoria();
-
-        /*TO-DO:
-        * Poder añadir otra categoria
-        * Añadir contacto*/
-    }
-
-    private void InicializarVariables(){
-        Uid_Usuario = findViewById(R.id.Uid_Usuario);
-        Correo_Usuario= findViewById(R.id.Correo_Usuario);
-        Fecha_hora_actual= findViewById(R.id.Fecha_hora_actual);
-        Fecha = findViewById(R.id.Fecha);
-        Hora = findViewById(R.id.Hora);
-        Estado = findViewById(R.id.Estado);
-
-        Titulo = findViewById(R.id.Titulo);
-        Descripcion = findViewById(R.id.Descripcion);
-        Btn_Calendario = findViewById(R.id.Btn_Calendario);
-        Btn_Hora = findViewById(R.id.Btn_Hora);
-
-        SpinnerNotificacion = findViewById(R.id.SpinnerNotificacion);
-        SpinnerCategoria = findViewById(R.id.SpinnerCategoria);
-    }
-
-    private void ObtenerDatos(){
-        String uid_recuperado = getIntent().getStringExtra("Uid");
-        String correo_recuperado = getIntent().getStringExtra("Correo");
-
-        Uid_Usuario.setText(uid_recuperado);
-        Correo_Usuario.setText(correo_recuperado);
-    }
-
-    private void Obtener_Fecha_Hora_Actual(){
-        String fecha_hora_registro = new SimpleDateFormat("dd-MM-yyyy/HH:mm:ss a",
-                Locale.getDefault()).format(System.currentTimeMillis());
-        Fecha_hora_actual.setText(fecha_hora_registro);
     }
 
     private void EstablecerNotificacion() {
@@ -199,6 +266,32 @@ public class Agregar_Nota extends AppCompatActivity {
         SpinnerCategoria.setAdapter(categoriaAdapter);
     }
 
+    private void Obtener_Contacto(){
+        Btn_Contactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactoIntent = new Intent(Intent.ACTION_PICK);
+                contactoIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(contactoIntent, REQUEST_CONTACT);
+            }
+        });
+    }
+
+    @SuppressLint("Range")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==REQUEST_CONTACT && resultCode==RESULT_OK){
+            Uri contactoUri = data.getData();
+            Cursor cursor = getContentResolver().query(contactoUri, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},null,null,null);
+            if(cursor!=null && cursor.moveToFirst()){
+                ContactoSeleccionado = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                Contacto.setText(ContactoSeleccionado);
+                cursor.close();
+            }
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -209,7 +302,7 @@ public class Agregar_Nota extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId()==R.id.Agregar_Nota_BD){
-            Toast.makeText(this, "Nota agregada", Toast.LENGTH_SHORT).show();
+            Guardar_Nota();
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/agenda/Objetos/Nota.java
+++ app/src/main/java/com/example/agenda/Objetos/Nota.java
@@ -2,6 +2,119 @@ package com.example.agenda.Objetos;
 
 public class Nota {
 
-    String id_nota, uid_usuario, correo, fecha_hora_actual, titulo, descripcion, fecha_nota, estado;
+    String id_nota, uid_usuario, correo, fecha_hora_actual, titulo, descripcion, fecha_nota, hora_nota, notificacion, categoria, contacto, estado;
 
+    public Nota() {
+    }
+
+    public Nota(String id_nota, String uid_usuario, String correo, String fecha_hora_actual, String titulo, String descripcion, String fecha_nota, String hora_nota, String notificacion, String categoria, String contacto, String estado) {
+        this.id_nota = id_nota;
+        this.uid_usuario = uid_usuario;
+        this.correo = correo;
+        this.fecha_hora_actual = fecha_hora_actual;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha_nota = fecha_nota;
+        this.hora_nota = hora_nota;
+        this.notificacion = notificacion;
+        this.categoria = categoria;
+        this.contacto = contacto;
+        this.estado = estado;
+    }
+
+    public String getId_nota() {
+        return id_nota;
+    }
+
+    public void setId_nota(String id_nota) {
+        this.id_nota = id_nota;
+    }
+
+    public String getUid_usuario() {
+        return uid_usuario;
+    }
+
+    public void setUid_usuario(String uid_usuario) {
+        this.uid_usuario = uid_usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFecha_hora_actual() {
+        return fecha_hora_actual;
+    }
+
+    public void setFecha_hora_actual(String fecha_hora_actual) {
+        this.fecha_hora_actual = fecha_hora_actual;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha_nota() {
+        return fecha_nota;
+    }
+
+    public void setFecha_nota(String fecha_nota) {
+        this.fecha_nota = fecha_nota;
+    }
+
+    public String getHora_nota() {
+        return hora_nota;
+    }
+
+    public void setHora_nota(String hora_nota) {
+        this.hora_nota = hora_nota;
+    }
+
+    public String getNotificacion() {
+        return notificacion;
+    }
+
+    public void setNotificacion(String notificacion) {
+        this.notificacion = notificacion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
