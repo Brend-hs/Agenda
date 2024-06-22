0da431acc9e810371cd24e998a99077545c858d4
MarianaBHdz <mariana.berenice2002@gmail.com>
2024-06-22 17:00:41 -0600

    Listar nota finalizado

--- app/src/main/java/com/example/agenda/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/example/agenda/ListarNotas/Listar_Notas.java
@@ -1,16 +1,38 @@
 package com.example.agenda.ListarNotas;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.agenda.Objetos.Nota;
 import com.example.agenda.R;
+import com.example.agenda.ViewHolder.ViewHolder_Nota;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 public class Listar_Notas extends AppCompatActivity {
+
+    RecyclerView recyclerViewNotas;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference BASE_DE_DATOS;
+
+    LinearLayoutManager linearLayoutManager;
+    FirebaseRecyclerAdapter<Nota, ViewHolder_Nota> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Nota> options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +44,80 @@ public class Listar_Notas extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Mis Tareas");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        recyclerViewNotas = findViewById(R.id.recyclerViewNotas);
+        recyclerViewNotas.setHasFixedSize(true); //Adapte su tamaño a los cmabios de la lista
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
+        ListarNotasUsuarios();
+    }
+
+    private void ListarNotasUsuarios(){
+        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(BASE_DE_DATOS,Nota.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_Nota>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder_Nota viewHolder_nota, int i, @NonNull Nota nota) {
+                viewHolder_nota.SetearDatos(
+                        getApplicationContext(),
+                        nota.getId_nota(),
+                        nota.getUid_usuario(),
+                        nota.getCorreo(),
+                        nota.getFecha_hora_actual(),
+                        nota.getTitulo(),
+                        nota.getDescripcion(),
+                        nota.getFecha_nota(),
+                        nota.getHora_nota(),
+                        nota.getNotificacion(),
+                        nota.getCategoria(),
+                        nota.getContacto(),
+                        nota.getEstado()
+                );
+            }
+
+            @Override
+            public ViewHolder_Nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota,parent,false);
+                ViewHolder_Nota viewHolder_nota = new ViewHolder_Nota(view);
+                viewHolder_nota.setOnClickListener(new ViewHolder_Nota.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "On item Click", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "On item long Click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return viewHolder_nota;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL,false);
+        linearLayoutManager.setReverseLayout(true);//Diseño inverso: del ultimo registro al primero
+        linearLayoutManager.setStackFromEnd(true);//El recycler view empiece desde la parte superior
+
+        recyclerViewNotas.setLayoutManager(linearLayoutManager);
+        recyclerViewNotas.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(firebaseRecyclerAdapter!=null){
+            firebaseRecyclerAdapter.startListening();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/example/agenda/ViewHolder/ViewHolder_Nota.java
@@ -0,0 +1,79 @@
+package com.example.agenda.ViewHolder;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.agenda.R;
+
+public class ViewHolder_Nota extends RecyclerView.ViewHolder {
+
+    View mView;
+
+    private ViewHolder_Nota.ClickListener mClickListener;
+    public  interface ClickListener{
+        void onItemClick(View view, int position);//Se ejecuta al presionar en el item
+        void onItemLongClick(View view, int position);//Se ejecuta al mantener presiona el item
+    }
+
+    public void  setOnClickListener(ViewHolder_Nota.ClickListener clickListener){
+        mClickListener = clickListener;
+    }
+
+    public ViewHolder_Nota(@NonNull View itemView) {
+        super(itemView);
+        mView = itemView;
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mClickListener.onItemClick(v,getAdapterPosition());
+            }
+        });
+
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mClickListener.onItemLongClick(v,getAdapterPosition());
+                return false;
+            }
+        });
+    }
+
+    public void SetearDatos(Context context, String id_nota, String uid_usuario, String correo_usuario,
+                            String fecha_hora_registro, String titulo, String descripcion, String fecha_nota,
+                            String hora_nota, String notificacion, String categoria, String contacto, String estado){
+        //Declarar vistas
+        TextView Id_nota_Item, Uid_Usuario_Item, Correo_Usuario_Item, Fecha_hora_registro_Item, Titulo_Item,
+                Descripcion_Item, Notificacion_Item, Fecha_Item, Hora_Item, Categoria_Item, Estado_Item;
+
+        //Establecer la conexion con el item
+        Id_nota_Item = mView.findViewById(R.id.Id_nota_Item);
+        Uid_Usuario_Item = mView.findViewById(R.id.Uid_Usuario_Item);
+        Correo_Usuario_Item = mView.findViewById(R.id.Correo_Usuario_Item);
+        Fecha_hora_registro_Item = mView.findViewById(R.id.Fecha_hora_registro_Item);
+        Titulo_Item = mView.findViewById(R.id.Titulo_Item);
+        Descripcion_Item = mView.findViewById(R.id.Descripcion_Item);
+        Notificacion_Item = mView.findViewById(R.id.Notificacion_Item);
+        Fecha_Item = mView.findViewById(R.id.Fecha_Item);
+        Hora_Item = mView.findViewById(R.id.Hora_Item);
+        Categoria_Item = mView.findViewById(R.id.Categoria_Item);
+        Estado_Item = mView.findViewById(R.id.Estado_Item);
+
+        //Setear la informacion dentro del item
+        Id_nota_Item.setText(id_nota);
+        Uid_Usuario_Item.setText(uid_usuario);
+        Correo_Usuario_Item.setText(correo_usuario);
+        Fecha_hora_registro_Item.setText(fecha_hora_registro);
+        Titulo_Item.setText(titulo);
+        Descripcion_Item.setText(descripcion);
+        Notificacion_Item.setText(notificacion);
+        Fecha_Item.setText(fecha_nota);
+        Hora_Item.setText(hora_nota);
+        Categoria_Item.setText(categoria);
+        Estado_Item.setText(estado);
+    }
+}
